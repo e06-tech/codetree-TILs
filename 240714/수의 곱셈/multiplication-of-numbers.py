038b71f6bf32888b8295b46356f42ac17f6704b9
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-07-14 21:01:28 +0900

    Title: 수의 곱셈, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/수의 곱셈/multiplication-of-numbers.py
+++ 240714/수의 곱셈/multiplication-of-numbers.py
@@ -7,12 +7,12 @@ D = x*y*z
 #이렇게 지정 안 하고 cal리스트 안에 계산식을 그대로 집어넣을 수도 있다.
 #하지만 실수할 가능성이 높아지니 능숙해지기 전까지는 안 그러는 걸로.
 
-cal = [A, B, C, D]
+cal = [x, y, z, A, B, C, D]
 
-odd = [0]*4
-even = [0]*4
+odd = [0]*len(cal)
+even = [0]*len(cal)
 
-for i in range(4):
+for i in range(len(cal)):
     if cal[i]%2 == 1:
         odd[i] = cal[i]
     else:
