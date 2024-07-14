afe0e0f4c28dcf087d19ea9f0860d87d433e4e02
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-07-14 20:58:58 +0900

    Title: 수의 곱셈, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240714/수의 곱셈/multiplication-of-numbers.py
@@ -0,0 +1,24 @@
+x, y, z = map(int, input().split())
+
+A = x*y
+B = x*z
+C = y*z
+D = x*y*z
+#이렇게 지정 안 하고 cal리스트 안에 계산식을 그대로 집어넣을 수도 있다.
+#하지만 실수할 가능성이 높아지니 능숙해지기 전까지는 안 그러는 걸로.
+
+cal = [A, B, C, D]
+
+odd = [0]*4
+even = [0]*4
+
+for i in range(4):
+    if cal[i]%2 == 1:
+        odd[i] = cal[i]
+    else:
+        even[i] = cal[i]
+
+if max(odd) == 0:
+    print(max(even))
+else:
+    print(max(odd))
