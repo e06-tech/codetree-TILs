9bc159d7b791c3782c2029c6626ddc0b91108553
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-07-18 23:51:22 +0900

    Title: 주사위 놀이, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/주사위 놀이/play-with-dice.py
+++ 240718/주사위 놀이/play-with-dice.py
@@ -9,3 +9,5 @@ for elem in result:
 for i in range(1, 7):
     cnt = result_array[i]
     print(i, "-" ,cnt)
+    #문제에서는 다른 print함수 형식의 코드로 작성함.
+    #다만, 여기서는 그 형식으로 작성했을 시 에러가 발생해서 정수와 str을 혼합하는 다른 방식으로 작성.
