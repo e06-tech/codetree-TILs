d1487fa2cfd4306acb3f1bec8f529cbcc95ffcad
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-07-31 23:05:55 +0900

    Title: 특정 위치의 문자, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/특정 위치의 문자/char-in-specific-location.py
@@ -0,0 +1,13 @@
+T = ['L', 'E', 'B', 'R', 'O', 'S']
+
+letter = input()
+idx = -1 #일치하는 문자가 없을 시 -1 출력
+
+for i, char in enumerate(T):
+    if char == letter:
+        idx = i
+        print(idx)
+        break
+    else:
+        print('None')
+        break
