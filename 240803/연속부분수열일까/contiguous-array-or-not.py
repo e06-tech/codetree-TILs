ab7f181bb87343122eaeaa0ec2e42df1073c694a
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-08-03 23:53:03 +0900

    Title: 연속부분수열일까, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/연속부분수열일까/contiguous-array-or-not.py
+++ 240803/연속부분수열일까/contiguous-array-or-not.py
@@ -1,10 +1,14 @@
+n1, n2 = map(int, input().split())
+#사실 n1, n2를 거의 쓸 일은 없지만..
+#for 문에서 넣을까 했으나 통일성 있게 코드를 짜는 과정에서 쓰지 않았다.
+
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 #일단 리스트 A, B 완성
 
 flag = 0
 for i in range(len(A) - len(B)+1):
-    A[i:i+len(B)] = mod_A
+    mod_A = A[i:i+len(B)]
     if mod_A == B:
         flag = 1
         break
