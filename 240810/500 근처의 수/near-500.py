ba3c139727258474e4946aa659858d70df496e71
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-08-10 03:44:18 +0900

    Title: 500 근처의 수, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240810/500 근처의 수/near-500.py
@@ -0,0 +1,16 @@
+numbers = list(map(int, input().split()))
+
+below = 0
+above = []
+
+for elem in numbers:
+    # 500 미만의 수 중 가장 큰 수 찾기
+    if below < elem and elem < 500:
+        below = elem
+
+for elem in numbers:
+    # 500 초과의 수 중 가장 큰 수 찾기
+    if elem > 500:
+        above.append(elem)
+
+print(below, min(above))
