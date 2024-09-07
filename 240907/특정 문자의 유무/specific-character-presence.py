4fc1f703522de8267187a655b7cd8b0a41e70c99
e06-tech <71258352+e06-tech@users.noreply.github.com>
2024-09-07 23:30:50 +0900

    Title: 특정 문자의 유무, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240907/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,18 @@
+words = input()
+
+l = len(words)
+answer = ['No', 'No']
+
+for i in range(l-1):
+    if words[i:i+2] == 'ee':
+        answer[0] = 'Yes'
+    else:
+        pass
+
+for i in range(l-1):
+    if words[i:i+2] == 'ab':
+        answer.append('Yes')
+    else:
+        pass
+
+print(*answer)
